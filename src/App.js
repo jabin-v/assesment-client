aeb346a6ae2fd8fb0fb78350d74b8cde1f3c5bfc
jabin <jabinv03@gmail.com>
2022-10-14 13:52:48 +0530

    add login and redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/login/Login';
+import Stock from './pages/stock/Stock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+    <Route path="/" >
+    <Route index element={<Login />} />
+    <Route path="stock" element={<Stock />} />
+    </Route>
+   </Routes>
   );
 }
 
